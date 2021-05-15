3f06d3df2db3bc2bae275c141d9d5d09e32a410b
Antonio Guzman <aguzmanv09@gmail.com>
2021-05-15 17:01:20 -0500

    Agragando boton de favoritos a la product card

--- components/ProductCard/index.tsx
+++ components/ProductCard/index.tsx
@@ -1,20 +1,41 @@
 import Link from 'next/link';
+import { AiFillHeart } from 'react-icons/ai';
+import { FaOpencart, FaShippingFast } from 'react-icons/fa';
 import styles from './productCard.module.scss';
 
 const ProductCard = ({ name, image }: { name: string, image: string }) => {
   return (
-    <Link href='/products/12ab34'>
-      <a className={styles.container}>
-        <figure className={styles.figure}>
-          <img src={image} alt={name} />
-        </figure>
-        <p>{name}</p>
-        <div className={styles.body}>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <button type='button' className='btn-link-acent'>
+          <AiFillHeart size={30} />
+        </button>
+        <Link href='/products/12ab34'>
+          <a className={styles.link}>
+            <figure className={styles.figure}>
+              <img src={image} alt={name} />
+            </figure>
+          </a>
+        </Link>
+      </div>
+      <p><strong>{name}</strong></p>
+      <div className={styles.body}>
+        <div className={styles.info}>
           <p><strong>Price: $350</strong></p>
           <small>(10)</small>
         </div>
-      </a>
-    </Link>
+        <div className={styles.actions}>
+          <button type='button' className='btn-acent' title='Comprar ahora'>
+            <FaShippingFast size={20} />
+            <span>Comprar ahora</span>
+          </button>
+          <button type='button' className='btn-success' title='Agregar al carrito'>
+            <FaOpencart size={20} />
+            <span>Agragar al carrito</span>
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
